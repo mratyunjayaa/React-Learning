c2c90ea5d580b238b5bbf7e00bfb43b7a258a947
Vishal TomaR <vishaltomarx13@gmail.com>
2026-06-18 16:23:45 +0530

    Add Header

---
+++ Day03/Components/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        <>
+        <div id="heading">
+        <h1>
+            GitHUb Profile Viewer
+        </h1>
+        </div>
+        </>
+    )
+}
+console.warn("ahm chal rhe ??")
+export default Header;
